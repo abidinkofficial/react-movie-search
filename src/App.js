c531ea797419c129ed380e08cbe0936437e11765
Abidin K <abidinkofficial@gmail.com>
2021-07-22 15:03:44 +0300

    Header section implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-function App() {
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+
+import Home from "./pages/Home"
+import Movie from "./pages/Movie"
+
+const App = () => {
   return (
-    <div className="App">
-      Movie Search
+    <div className="font-sans antialiased text-gray-500">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/movie/:id">
+            <Movie />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
