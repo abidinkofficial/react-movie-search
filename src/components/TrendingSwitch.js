ea325a1d41504cf12229d0ba1aa5acf88a57c8b4
Abidin K <abidinkofficial@gmail.com>
2021-07-23 14:14:35 +0300

    Card layout implemented with dummy data

--- src/components/TrendingSwitch.js
+++ src/components/TrendingSwitch.js
@@ -1,8 +1,8 @@
 const TrendingSwitch = () => {
   return (
-    <div className="w-max bg-gray-200 rounded-full overflow-hidden flex text-gray-900 ">
+    <div className="w-max bg-gray-100 rounded-full overflow-hidden flex text-gray-900 shadow-md">
       <button className="px-3 py-1 bg-indigo-800 rounded-full text-gray-50">Today</button>
-      <button className="px-3 py-1 rounded-full hover:bg-gray-300 transition-colors">This Week</button>
+      <button className="px-3 py-1 rounded-full hover:bg-gray-200 transition-colors">This Week</button>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header"
 import Search from "../components/Search"
 import TrendingSwitch from "../components/TrendingSwitch"
+import Card from "../components/Card"
 
 const Home = () => {
   return (
@@ -10,6 +11,12 @@ const Home = () => {
         <Search />
         <div className="mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-16 py-5">
           <TrendingSwitch />
+          <div className="flex flex-wrap justify-center">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
         </div>
       </main>
     </>
