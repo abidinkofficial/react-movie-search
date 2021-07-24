f34b6064fbffe7ea4da23ea884325901b02e340a
Abidin K <abidinkofficial@gmail.com>
2021-07-24 14:29:04 +0300

    Trendings switch functionality implemented

--- src/components/TrendingSwitch.js
+++ src/components/TrendingSwitch.js
@@ -1,8 +1,16 @@
-const TrendingSwitch = () => {
+const TrendingSwitch = ({ trendingsTime, setTrendingsTime }) => {
+
+  const changeTrendingsTime = (newTrendingsTime) => {
+    setTrendingsTime(newTrendingsTime)
+  }
+
+  const activeClassName = "bg-indigo-800 text-gray-50"
+  const passiveClassName = "hover:bg-gray-200 transition-colors"
+
   return (
     <div className="w-max bg-gray-100 rounded-full overflow-hidden flex text-gray-900 shadow-md">
-      <button className="px-3 py-1 bg-indigo-800 rounded-full text-gray-50">Today</button>
-      <button className="px-3 py-1 rounded-full hover:bg-gray-200 transition-colors">This Week</button>
+      <button onClick={() => changeTrendingsTime("day")} className={`px-3 py-1 rounded-full ${trendingsTime === "day" ? activeClassName : passiveClassName}`}>Today</button>
+      <button onClick={() => changeTrendingsTime("week")} className={`px-3 py-1 rounded-full ${trendingsTime === "day" ? passiveClassName : activeClassName}`}>This Week</button>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,13 +8,15 @@ import TrendingSwitch from "../components/TrendingSwitch"
 import Card from "../components/Card"
 
 const Home = ({ dispatch, trendingsLoading, trendings, trendingsHasErrors }) => {
-  const [trendingsCards, setTrendingsCards] = useState([]);
+  const [trendingsCards, setTrendingsCards] = useState([])
+  const [trendingsTime, setTrendingsTime] = useState("day")
 
   useEffect(() => {
-    dispatch(fetchTrendings())
-  }, [dispatch])
+    dispatch(fetchTrendings(trendingsTime))
+  }, [dispatch, trendingsTime])
 
   useEffect(() => {
+    setTrendingsCards([])
     trendings?.results?.map((result) => setTrendingsCards((trendingsCards) => [...trendingsCards, <Card movie={result} key={result.id} />]))
   }, [trendings])
 
@@ -26,7 +28,7 @@ const Home = ({ dispatch, trendingsLoading, trendings, trendingsHasErrors }) => 
         <div className="mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-16 py-5">
           <div className="flex my-5">
             <h2 className="text-2xl font-semibold mr-5">Trending movies:</h2>
-            <TrendingSwitch />
+            <TrendingSwitch trendingsTime={trendingsTime} setTrendingsTime={setTrendingsTime} />
           </div>
           <div className="flex flex-wrap justify-center">
             {trendingsCards}
