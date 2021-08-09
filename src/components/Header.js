5bad2185bb3da5d02b895072933a0461bb151dd7
Abidin K <abidinkofficial@gmail.com>
2021-08-09 11:40:13 +0300

    GitHub button redesigned

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ const Header = ({ context, refresh }) => {
         <header className="bg-gradient-to-r from-indigo-900 to-indigo-700 px-4 sm:px-6 lg:px-16 pt-8">
           <div className="mx-auto max-w-screen-2xl flex items-center justify-between border-b border-gray-900 border-opacity-30 py-4">
             <div onClick={refresh} className="cursor-pointer"><img src={process.env.PUBLIC_URL + "/logo-light.svg"} alt="movie search logo" /></div>
-            <a className="github-button" href="https://github.com/abidinkofficial" data-size="large" aria-label="Follow @abidinkofficial on GitHub">Follow @abidinkofficial</a>
+            <a href="https://github.com/abidinkofficial/react-movie-search" target="_blank" rel="noreferrer" className="flex items-center justify-center bg-gray-50 hover:bg-gray-200 rounded-md hover:shadow-md py-4 px-8 text-sm text-gray-800 font-semibold transition-colors h-10"><span style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/github-64.png)`, backgroundSize: "1.25rem", backgroundRepeat: "no-repeat", backgroundPosition: "center" }} className="w-6 h-6 mr-2"></span>View on GitHub</a>
           </div>
           <div className="mx-auto max-w-screen-2xl flex items-center justify-between py-8">
             <h1 className="text-2xl font-semibold text-gray-100">Search movies by typing below, or bring a random one.</h1>
@@ -32,7 +32,7 @@ const Header = ({ context, refresh }) => {
         <header className="bg-gradient-to-r from-indigo-900 to-indigo-700 px-4 sm:px-6 lg:px-16">
           <div className="mx-auto max-w-screen-2xl flex items-center justify-between py-4">
             <a href="/"><img src={process.env.PUBLIC_URL + "/logo-light.svg"} alt="movie search logo" /></a>
-            <a className="github-button" href="https://github.com/abidinkofficial" data-size="large" aria-label="Follow @abidinkofficial on GitHub">Follow @abidinkofficial</a>
+            <a href="https://github.com/abidinkofficial/react-movie-search">View on GitHub</a>
           </div>
         </header>}
     </>
